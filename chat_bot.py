325db635d7ba49c1862a47a483b08a60b9b2e5db
samruddha <samruddhakumbhar@gmail.com>
2024-05-06 15:55:59 +0530

    done

--- chat_bot.py
+++ chat_bot.py
@@ -1,8 +1,5 @@
 from langchain.prompts import PromptTemplate
-from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.memory import ChatMessageHistory
 from llms import llm
 import vectordb
 
